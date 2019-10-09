02d59ec72b262df210f20d9f78163bcec4540098
Slymaster <sylvain.rey.pro@gmail.com>
2019-10-09 09:25:46 +0200

    Color assigned according to pokemon type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,22 @@ import { OnInit } from '@angular/core';
 
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
+import Swal from 'sweetalert2';
+/*
+import {all} from "codelyzer/util/function";
+*/
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'pokemon',
   templateUrl: './pokemon.html',
 })
 
 export class AppComponent implements OnInit {
-  private title: string = "Pokemons"
+  private title = 'Pokemons';
   protected pokemons: Pokemon[];
-  private value: string ='';
-  private age: number = 19;
+  private value = '';
+  private age = 19;
   private showInfos: boolean;
   private pic: string;
   private hp: number;
@@ -21,44 +26,29 @@ export class AppComponent implements OnInit {
   private types: Array<string>;
   private name: string;
 
-  /**
-   * Permet d'obtenir la couleur d'un type de pokemon
-   * @param pokemon le pokemon correspondant
-   */
-  /*getColor(type:Array<string>) {
-    switch(type) {
-      case "Plante":
-        return "green";
-      case "Feu":
-        return "red";
-      case "Eau":
-        return "blue";
-      case "Insecte":
-        return "lime";
-      case "Normal":
-        return "cyan darken-4";
-      case "Electrik":
-        return "yellow";
-      case "Poison":
-        return "indigo"
-      case "Vol":
-        return "cyan lighten-5"
-      case "Fée":
-        return "blue accent-1"
-    }
-
+/*  showModal(){
+    Swal({
+      title: 'Success',
+      text: 'Nice',
+      type: 'error'
+    })
   }*/
 
 
-  onClick(pokemon:Pokemon) {
+  onClick(pokemon: Pokemon) {
+    /*
     this.name = pokemon.name;
     this.pic = pokemon.picture;
     this.hp = pokemon.hp;
     this.cp = pokemon.cp;
     this.types = pokemon.types;
+    */
+    /*let myDialog:any = <any>document.getElementById("myDialog");
+    myDialog.showModal();*/
   }
 
   ngOnInit(): void {
+
     this.pokemons = POKEMONS;
   }
 
@@ -73,4 +63,34 @@ export class AppComponent implements OnInit {
     this.value = 'Bonjour ' + event.target.value;
   }*/
 }
+
+
+/**
+ * Permet d'obtenir la couleur d'un type de pokemon
+ * @param pokemon le pokemon correspondant
+ */
+/*getColor(type:Array<string>) {
+  switch(type) {
+    case "Plante":
+      return "green";
+    case "Feu":
+      return "red";
+    case "Eau":
+      return "blue";
+    case "Insecte":
+      return "lime";
+    case "Normal":
+      return "cyan darken-4";
+    case "Electrik":
+      return "yellow";
+    case "Poison":
+      return "indigo"
+    case "Vol":
+      return "cyan lighten-5"
+    case "Fée":
+      return "blue accent-1"
+  }
+
+}*/
+
 
