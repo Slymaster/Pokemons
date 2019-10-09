280c1368f2c39618a795ded56ee3fb7503852080
Slymaster <sylvain.rey.pro@gmail.com>
2019-10-09 11:26:59 +0200

    routes w/ nav, 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemons';
-import Swal from 'sweetalert2';
+import { Pokemon } from './pokemon/pokemon';
+import { POKEMONS } from './pokemon/mock-pokemons';
 /*
+import Swal from 'sweetalert2';
 import {all} from "codelyzer/util/function";
 */
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'pokemon',
-  templateUrl: './pokemon.html',
+  selector: 'pokemon-app',
+  templateUrl: './app.component.html',
 })
 
 export class AppComponent implements OnInit {
-  private title = 'Pokemons';
-  protected pokemons: Pokemon[];
-  private value = '';
-  private age = 19;
-  private showInfos: boolean;
-  private pic: string;
-  private hp: number;
-  private cp: number;
-  private types: Array<string>;
-  private name: string;
+  ngOnInit(): void {
+  }
+}
+
+/*private title = 'Pokemons';
+protected pokemons: Pokemon[];
+private value = '';
+private age = 19;
+private showInfos: boolean;
+private pic: string;
+private hp: number;
+private cp: number;
+private types: Array<string>;
+private name: string;*/
 
 /*  showModal(){
     Swal({
@@ -35,22 +37,25 @@ export class AppComponent implements OnInit {
   }*/
 
 
-  onClick(pokemon: Pokemon) {
-    /*
-    this.name = pokemon.name;
-    this.pic = pokemon.picture;
-    this.hp = pokemon.hp;
-    this.cp = pokemon.cp;
-    this.types = pokemon.types;
-    */
-    /*let myDialog:any = <any>document.getElementById("myDialog");
-    myDialog.showModal();*/
-  }
+/*onClick(pokemon: Pokemon) {}*/
+  /*
+  this.name = pokemon.name;
+  this.pic = pokemon.picture;
+  this.hp = pokemon.hp;
+  this.cp = pokemon.cp;
+  this.types = pokemon.types;
+  */
+  /*let myDialog:any = <any>document.getElementById("myDialog");
+  myDialog.showModal();*/
 
-  ngOnInit(): void {
 
-    this.pokemons = POKEMONS;
-  }
+/*
+ngOnInit(): void {
+
+  this.pokemons = POKEMONS;
+}
+}
+*/
 
 /*
   selectPokemon(pokemon:Pokemon) {
@@ -62,7 +67,7 @@ export class AppComponent implements OnInit {
     console.log("ok");
     this.value = 'Bonjour ' + event.target.value;
   }*/
-}
+
 
 
 /**
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { PokemonTypeColorPipe } from './pokemon/pokemon-type-color.pipe';
+import { ListPokemonComponent } from './pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonTypeColorPipe
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
